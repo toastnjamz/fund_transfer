d041f048e58661049601e97194cb5064e4d0e242
toastnjamz <veritydavis@gmail.com>
2020-04-24 17:17:13 -0700

    adding home page html and css files

--- src/main/java/com/paymybuddy/fund_transfer/controller/UserController.java
+++ src/main/java/com/paymybuddy/fund_transfer/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.paymybuddy.fund_transfer.controller;
 
 import com.paymybuddy.fund_transfer.domain.User;
+import com.paymybuddy.fund_transfer.repository.UserRepository;
 import com.paymybuddy.fund_transfer.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,8 @@ import java.util.List;
 public class UserController {
 
     private UserService userService;
+    //TODO: delete
+    private UserRepository userRepository;
 
     @Autowired
     public UserController(UserService userService) {
@@ -29,11 +32,26 @@ public class UserController {
         return userService.findUserById(id);
     }
 
+    //TODO: delete test method
     @PostMapping("/user")
-    public User createUser(@Valid @RequestBody User user, HttpServletResponse response) {
+    public @ResponseBody String createUser(@RequestParam String email, @RequestParam String password,
+                                           @RequestParam String displayName, HttpServletResponse response) {
+
+        User testUser = new User();
+        testUser.setEmail(email);
+        testUser.setPassword(password);
+        testUser.setDisplayName(displayName);
+
+        userRepository.save(testUser);
         response.setStatus(201);
-        return userService.createUser(user);
+        return "User Created";
     }
+
+//    @PostMapping("/user")
+//    public User createUser(@Valid @RequestBody User user, HttpServletResponse response) {
+//        response.setStatus(201);
+//        return userService.createUser(user);
+//    }
 
     @PutMapping("/user")
     public void updateUser(@Valid @RequestBody User user, HttpServletResponse response) {
